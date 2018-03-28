4b7445cfcc0d245e6ca6d51b44890fb3c092d6b1
Kevin Cunnane <kevcunnane@gmail.com>
2018-03-28 15:07:14 -0700

    Update with change to sqlops-extension-sqlops for sqlops npm repo

--- generators/app/env.js
+++ generators/app/env.js
@@ -4,13 +4,19 @@
 'use strict';
 var request = require('request');
 
-var vscodeFallbackVersion = '^1.20.0';
+var vscodeFallbackVersion = '^1.19.0';
 var vscodePromise = new Promise(function(resolve, reject) {
     request.get('https://vscode-update.azurewebsites.net/api/releases/stable', { headers: { "X-API-Version": "2" } }, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             try {
                 var tagsAndCommits = JSON.parse(body);
                 if (Array.isArray(tagsAndCommits) && tagsAndCommits.length > 0) {
+                    var segments = tagsAndCommits[0].version.split('.');
+                    if (tagsAndCommits.length > 3) {
+                        // Ops Studio is usually a couple of versions behind so
+                        // should use the correct version
+                        var segments = tagsAndCommits[2].version.split('.');
+                    }
                     var segments = tagsAndCommits[0].version.split('.');
                     if (segments.length === 3) {
                         resolve('^' + segments[0] + '.' + segments[1] + '.0');
