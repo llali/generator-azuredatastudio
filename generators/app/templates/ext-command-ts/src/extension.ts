23080868da629e37257c1f30f870988691207b15
Leila Lali <llali@microsoft.com>
2018-04-25 16:22:26 -0700

    added a simple example for using sqlops

--- generators/app/templates/ext-command-ts/src/extension.ts
+++ generators/app/templates/ext-command-ts/src/extension.ts
@@ -20,14 +20,24 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
-    });
+    }));
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('extension.showCurrentConnection', () => {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        sqlops.connection.getCurrentConnection().then(connection => {
+            let connectionId = connection ? connection.connectionId : 'No connection found!';
+            vscode.window.showInformationMessage(connectionId);
+        }, error => {
+             console.info(error);
+        });
+    }));
 }
 
 // this method is called when your extension is deactivated
