efe03bfa3f2896fe4a2115e4cb44fa29a1343285
Drew Skwiers-Koballa <drew.koballa@gmail.com>
2019-11-17 20:20:42 -0600

    code vscode version in current ADS

--- generators/app/env.js
+++ generators/app/env.js
@@ -6,22 +6,13 @@ var request = require('request');
 
 var vscodeFallbackVersion = '^1.19.0';
 var vscodePromise = new Promise(function(resolve, reject) {
-    request.get('https://vscode-update.azurewebsites.net/api/releases/stable', { headers: { "X-API-Version": "2" } }, function(error, response, body) {
+    request.get('https://raw.githubusercontent.com/microsoft/azuredatastudio/master/product.json', {}, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             try {
-                var tagsAndCommits = JSON.parse(body);
-                if (Array.isArray(tagsAndCommits) && tagsAndCommits.length > 0) {
-                    var segments = tagsAndCommits[0].version.split('.');
-                    if (tagsAndCommits.length > 3) {
-                        // Ops Studio is usually a couple of versions behind so
-                        // should use the correct version
-                        var segments = tagsAndCommits[2].version.split('.');
-                    }
-                    var segments = tagsAndCommits[0].version.split('.');
-                    if (segments.length === 3) {
-                        resolve('^' + segments[0] + '.' + segments[1] + '.0');
-                        return;
-                    }
+                var currentAzureDataStudio = JSON.parse(body);
+                if (currentAzureDataStudio.vscodeVersion) {
+                    resolve('^'+currentAzureDataStudio.vscodeVersion);
+                    return;
                 }
             } catch (e) {
                 console.log('Problem parsing version: ' + body, e);
@@ -31,6 +22,31 @@ var vscodePromise = new Promise(function(resolve, reject) {
         }
         resolve(vscodeFallbackVersion);
     });
+    // request.get('https://vscode-update.azurewebsites.net/api/releases/stable', { headers: { "X-API-Version": "2" } }, function(error, response, body) {
+    //     if (!error && response.statusCode === 200) {
+    //         try {
+    //             var tagsAndCommits = JSON.parse(body);
+    //             if (Array.isArray(tagsAndCommits) && tagsAndCommits.length > 0) {
+    //                 var segments = tagsAndCommits[0].version.split('.');
+    //                 if (tagsAndCommits.length > 3) {
+    //                     // Ops Studio is usually a couple of versions behind so
+    //                     // should use the correct version
+    //                     var segments = tagsAndCommits[2].version.split('.');
+    //                 }
+    //                 var segments = tagsAndCommits[0].version.split('.');
+    //                 if (segments.length === 3) {
+    //                     resolve('^' + segments[0] + '.' + segments[1] + '.0');
+    //                     return;
+    //                 }
+    //             }
+    //         } catch (e) {
+    //             console.log('Problem parsing version: ' + body, e);
+    //         }
+    //     } else {
+    //         console.log('Unable to fetch latest vscode version: ' + (error || ('Status code: ' + response.statusCode + ', ' + body)));
+    //     }
+    //     resolve(vscodeFallbackVersion);
+    // });
 });
 
 module.exports.getLatestVSCodeVersion = function() { return vscodePromise; };
