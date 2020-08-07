31479d320d6b8cd1a2c3db2141ea9ad2af2ee12d
Laura Jiang <jianglau@oregonstate.edu>
2020-08-07 15:32:08 -0700

    address PR comments

--- generators/app/templates/ext-jupyterbook/src/jupyter-book.ts
+++ generators/app/templates/ext-jupyterbook/src/jupyter-book.ts
@@ -6,6 +6,9 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
 
+// This function looks in the user's default extensions folder for Azure Data Studio
+// to find this extension and its packaged files. If it encounters an error, a
+// message will appear in an error window.
 const processNotebooks = () => {
     const rootExtensionsFolder = path.normalize(path.join(os.homedir(), '.azuredatastudio', 'extensions'))
     let subExtensionFolder = getFolderContent(rootExtensionsFolder);
@@ -19,6 +22,9 @@ const processNotebooks = () => {
     }
 }
 
+// This function is called by processNotebooks to find the correct folder that contains
+// this extension. If it is found, then it opens up the book in Azure Data Studio's
+// native book viewlet.
 const findCorrectFolder = (folderName: string, rootExtensionsFolder: string) => {
     let folderExt = path.basename(folderName).toLowerCase();
 
@@ -28,6 +34,7 @@ const findCorrectFolder = (folderName: string, rootExtensionsFolder: string) => 
     }
 }
 
+// This is a wrapper to read each subfolder in the extensions folder.
 const getFolderContent = (folderPath: string) => {
     try {
         return fs.readdirSync(folderPath);
@@ -37,6 +44,9 @@ const getFolderContent = (folderPath: string) => {
     }
 }
 
+// This function is called when you run the command `Launch Book: <%= name %>` from
+// command palette in Azure Data Studio. If you would like any additional functionality
+// to occur when you launch the book, add to the activate function.
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('launchBook.<%= name %>', () => {
         processNotebooks();
--- generators/app/templates/ext-notebook/src/notebook.ts
+++ generators/app/templates/ext-notebook/src/notebook.ts
@@ -6,6 +6,9 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
 
+// This function looks in the user's default extensions folder for Azure Data Studio
+// to find this extension and its packaged files. If it encounters an error, a
+// message will appear in an error window.
 const processNotebooks = () => {
     const rootExtensionsFolder = path.normalize(path.join(os.homedir(), '.azuredatastudio', 'extensions'))
     let notebookNames: Array<string> = [];
@@ -18,13 +21,15 @@ const processNotebooks = () => {
     return notebookNames;
 }
 
+// This function is called by processNotebooks to find the correct folder that contains
+// this extension. If it is found, then it opens up the book in Azure Data Studio's
+// native notebook viewlet.
 const findCorrectFolder = (folderName: string, rootExtensionsFolder: string, notebookNames: Array<string>) => {
     let folderExt = path.basename(folderName).toLowerCase();
 
-    if (folderExt.indexOf('<%= name%>') > -1) {
+    if (folderExt.indexOf(('<%= publisherName%>.<%= name%>').toLowerCase()) > -1) {
         let fullFolderPath = path.join(rootExtensionsFolder, folderName);
         try {
-            //notebookNames.push(fullFolderPath); // open in side viewlet
             extractNotebooksFromFolder(fullFolderPath, notebookNames);
         } catch (e) {
             vscode.window.showErrorMessage("Unable to access " + fullFolderPath + ": " + e.message);
@@ -32,17 +37,20 @@ const findCorrectFolder = (folderName: string, rootExtensionsFolder: string, not
     }
 }
 
+// Each notebook or markdown file packaged with your extension will be found through this
+// function so that they can be individually opened through the `showNotebookDocument` command.
 const extractNotebooksFromFolder = (fullFolderPath: string, notebookNames: Array<string>) => {
     const files = getFolderContent(fullFolderPath);
     files.forEach(fileName => {
         let fileExtension = path.extname(fileName).toLowerCase();
-        if (fileExtension === '.ipynb'){
+        if (fileExtension === '.ipynb' || fileExtension === '.md') {
             let fullFilePath = path.join(fullFolderPath, fileName)
             notebookNames.push(path.normalize(fullFilePath));
         }
     })
 }
 
+// This is a wrapper to read each subfolder in the extensions folder.
 const getFolderContent = (folderPath: string) => {
     try {
         return fs.readdirSync(folderPath);
@@ -52,10 +60,12 @@ const getFolderContent = (folderPath: string) => {
     }
 }
 
+// This function is called when you run the command `Launch Notebooks: <%= name %>` from
+// command palette in Azure Data Studio. If you would like any additional functionality
+// to occur when you launch the book, add to the activate function.
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('launchNotebooks.<%= name %>', () => {
         let notebooksToDisplay: Array<string> = processNotebooks();
-        //vscode.commands.executeCommand('bookTreeView.openBook',notebooksToDisplay[0], true, notebooksToDisplay[1]); // open in side viewlet
         notebooksToDisplay.forEach(name => {
             azdata.nb.showNotebookDocument(vscode.Uri.file(name));
         });
